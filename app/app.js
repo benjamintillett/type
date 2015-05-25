ca72e22668ded4907e418253e131a6f6da92f23a
benjamin tillett <benjamin.tillett@gmail.com>
2015-05-25 12:42:43 +0100

    change port so it can pull from process.argv for heroku

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+app.set('port', (process.env.PORT || 3000));
+
 var app = express();
 
 app.get('/',function(req,res){
 	res.send("Hello");
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
