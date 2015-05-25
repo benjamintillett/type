9401d425e0faeac6b2e9efe3c0e865a8789e83fa
benjamin tillett <benjamin.tillett@gmail.com>
2015-05-25 18:58:13 +0100

    add jade and stylus and basic hompage and styling

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
 var express = require('express');
-
+var path 	= require('path');
+var stylus  = require('stylus');
 var app = express();
 
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.use(stylus.middleware(process.argv[3] || path.join(__dirname, 'public')));
 app.set('port', (process.env.PORT || 3000));
+
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname,'templates'));
+
 app.get('/',function(req,res){
-	res.send("Hello for codeship I canny believe this!?");
+	res.render('homepage',{title: 'typebook'});
 });
 
-
 module.exports = app;
+
