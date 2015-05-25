bc7b1155ae85c6049d22b625d7512ac20fcc6c88
benjamin tillett <benjamin.tillett@gmail.com>
2015-05-25 13:53:17 +0100

    chaged homepage text to check deployment to heroku via codeship

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.get('/',function(req,res){
-	res.send("Hello for codeship!?");
+	res.send("Hello for codeship I canny believe this!?");
 });
 
 
